34152b9dc73128cfb2c8e231ee2ab61d99c1e644
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-03-31 01:03:03 -0700

    [ADD] basic animation for rect 1

--- generative_logotype/p5/square_rr/sketch.js
+++ generative_logotype/p5/square_rr/sketch.js
@@ -18,13 +18,25 @@ function draw() {
   stroke(0);
   strokeWeight(4);
     
+  rect(0, 0, cir_dim, cir_dim); // outer, circumscribing rect
   ellipse((canvasSize+offset)/2, (canvasSize+offset)/2, canvasSize+offset, canvasSize+offset);
-  rect(0, 0, cir_dim, cir_dim); // better rect
 
   translate(offset*(sq_dim/cir_dim), offset*(sq_dim/cir_dim));
   print(mouseX, mouseY)
     
-  // rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // bigger rect - complique
-  // rect(0, 0, (sq_dim/cir_dim)*sq_dim, (sq_dim/cir_dim)*sq_dim); // better rect
+  // rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // inner rect - complique
   rect(0, 0, sq_dim, sq_dim); // better rect
+
+  // rect 1 (left top/rect2
+
+  // randomly select a bounding line for rect1
+  rect1_lower_bound_y = random(50, 300)
+  rect1_offset_from_top = random(0,rect1_lower_bound_y)
+  line(0, rect1_lower_bound_y, sq_dim, rect1_lower_bound_y);
+  rect1_height = rect1_lower_bound_y - rect1_offset_from_top
+  rect(0, rect1_offset_from_top, rect1_height, rect1_height);
+
+  // rect 2 (right rect1/rect3)
+  // rect 3 (bottom rect2)
+
 }
