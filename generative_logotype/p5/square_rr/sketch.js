75ce242554007ccde2166d4d4c5800b0071473b4
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-03-30 23:50:04 -0700

    [CLEANUP] offsets and weird math, get variables in order, for easier developement

--- generative_logotype/p5/square_rr/sketch.js
+++ generative_logotype/p5/square_rr/sketch.js
@@ -14,16 +14,17 @@ function setup() {
 
 function draw() {
 
-    background(255);
-    stroke(0);
-    strokeWeight(3);
+  background(255);
+  stroke(0);
+  strokeWeight(4);
     
   ellipse((canvasSize+offset)/2, (canvasSize+offset)/2, canvasSize+offset, canvasSize+offset);
-    
-    translate(offset*(sq_dim/cir_dim), offset*(sq_dim/cir_dim));
-    print(mouseX, mouseY)
-    
-  rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // bigger rect - complique
-  // rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // bigger rect
+  rect(0, 0, cir_dim, cir_dim); // better rect
 
+  translate(offset*(sq_dim/cir_dim), offset*(sq_dim/cir_dim));
+  print(mouseX, mouseY)
+    
+  // rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // bigger rect - complique
+  // rect(0, 0, (sq_dim/cir_dim)*sq_dim, (sq_dim/cir_dim)*sq_dim); // better rect
+  rect(0, 0, sq_dim, sq_dim); // better rect
 }
