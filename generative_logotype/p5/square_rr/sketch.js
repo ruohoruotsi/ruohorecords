b8b13ccd767e8763d51ab56b305c8168879fa4bd
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-04-01 06:07:25 -0700

    [CHECKPOINT] for wip

--- generative_logotype/p5/square_rr/sketch.js
+++ generative_logotype/p5/square_rr/sketch.js
@@ -16,18 +16,18 @@ function draw() {
 
   background(255);
   stroke(0);
-  strokeWeight(4);
+  strokeWeight(6);
     
   rect(0, 0, cir_dim, cir_dim); // outer, circumscribing rect
   ellipse((canvasSize+offset)/2, (canvasSize+offset)/2, canvasSize+offset, canvasSize+offset);
 
   translate(offset*(sq_dim/cir_dim), offset*(sq_dim/cir_dim));
-  print(mouseX, mouseY)
+  print("mouseX: " + mouseX, mouseY)
     
   // rect(cir_dim/18, cir_dim/18, sq_dim - 2*cir_dim/18, sq_dim - 2*cir_dim/18); // inner rect - complique
   rect(0, 0, sq_dim, sq_dim); // better rect
 
-  // rect 1 (left top/rect2
+  // rect 1 (left top/rect2)
 
   // randomly select a bounding line for rect1
   rect1_lower_bound_y = random(50, 300)
@@ -38,21 +38,24 @@ function draw() {
   rect(0, rect1_offset_from_top, rect1_height, rect1_height);
   noFill();
 
-  // rect 2 (right rect1/rect3)
-  rect2_left_bound_x = random(200, sq_dim);
-  rect2_height = sq_dim - rect2_left_bound_x;
+  // rect 2 (right rect1/rect3) # 200 needs to be anchored to whatever rect1 is doing
+  var gap = 50
+  rect2_left_bound_x = random(200, sq_dim - gap);
+  // fills out the rest of the x dimension (sq_dim), anchoring rect2 to right side
+  rect2_width = sq_dim - rect2_left_bound_x;  
   fill(100);
-  rect(rect2_left_bound_x, rect1_lower_bound_y, rect2_height, 50);
+  rect(rect2_left_bound_x, rect1_lower_bound_y, rect2_width, random(20,100));
   noFill();
 
 
   // rect 3 (bottom rect2)
   rect3_top_bound_y = sq_dim - 150; // random(sq_dim - 200, sq_dim);
   rect3_height = random(20,100);
-  rect3_width = 60; // random(40,60);
+  rect3_width = random(60,200);
   fill(100);
-  // rect((sq_dim - rect3_width)/2, sq_dim - rect3_height, (sq_dim + rect3_width)/2, rect3_height);
-  rect(200, 350, 250, 5);  // rect(x,y, w, h) ==> rect(30, 20, 55, 55);
+  // rect(x,y, w, h) ==> rect(30, 20, 55, 55);
+  rect((sq_dim - rect3_width)/2, sq_dim - rect3_height, rect3_width, rect3_height);
+
 
 
   noFill();
