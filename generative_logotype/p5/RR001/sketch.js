526916d6da5c5a7cdb13c61d2ad1c4da961a462e
ruohoruotsi <ruoho.ruotsi@gmail.com>
2020-04-10 10:15:10 -0700

    [ADD] more modular, replicable, parameterized drawOnes

--- generative_logotype/p5/RR001/sketch.js
+++ generative_logotype/p5/RR001/sketch.js
@@ -4,7 +4,7 @@
  *
  * p5.js: http://p5js.org/reference
  *
- * @author : ruoho ruotsi RR001
+ * @author : ruoho ruotsi
  */
 
 
@@ -12,40 +12,19 @@
 // setup
 // -----------------------------------------------------------------
 var multiplier = 1;
-var sq_dim = 800;
+var sq_dim = 60; // 20-60?
 var canvasSize_w = 800 * multiplier;  // 1080p at retina // 1920×1080 
 var canvasSize_h = 800 * multiplier;  // 1080p at retina // 1920×1080 
 
-var celln1 = 30;
+var celln1 = 4;
 var cells1 = [];
 
-// var celln2 = 13;
-// var cells2 = [];
-
 var DEBUG = false;  // control debug logging and diagnostic lines
-var margin = 40;
-
-let pg_bg;
-let pg_bg_img;
-
-let pg_text;
-let pg_text_img;
-  // var pg = createGraphics(canvasSize_w, canvasSize_h);
-  // //do stuff inside pg
-  // var img = createImage(pg.width, pg.height);
-  // // img.copy(pg, 0, 0, pg.width, pg.height, 0, 0, pg.width, pg.height);
-
-function preload() {
-  // font = loadFont('assets/Jangotype.ttf');
-  font = loadFont('assets/bombfact.ttf');
-  pg_text = createGraphics(canvasSize_w, canvasSize_h);
-  pg_bg = createGraphics(canvasSize_w, canvasSize_h);
-
-}
+var margin = sq_dim/18;
 
 function setup() {
   createCanvas(canvasSize_w, canvasSize_h);
-  frameRate(0.1); // Attempt to refresh at starting FPS // 0.5
+  frameRate(1); // Attempt to refresh at starting FPS // 0.5
 
   // setup cell grid1
   for (var i = 0; i < celln1; i++) {
@@ -55,14 +34,6 @@ function setup() {
           cells1[i][j].rand();
       }
   }
-
-  pg_text.textFont(font);
-  pg_text.textSize(300);
-  pg_text.textLeading(150);
-  // pg_text.textAlign(CENTER, CENTER);
-
-  pg_bg_img = createImage(canvasSize_w, canvasSize_h);
-  pg_text_img = createImage(canvasSize_w, canvasSize_h);
 
   noFill();
   if (DEBUG) {
@@ -74,65 +45,34 @@ function setup() {
 function draw() {
 
   // clear background on each frame
-  pg_bg.background(255, 255, 255);      // <=== white background
-  pg_bg.clear();                        // <=== transparent background
-
-  // bg cell grid color
-  pg_bg.stroke(99, 40, 37);
+  background(255, 255, 255);      // <=== white background
+  clear();                        // <=== transparent background
 
   // draw one here
   pos1_w = (canvasSize_w - sq_dim) / 2 ;
   pos_h = (canvasSize_h - sq_dim) / 2;
-  drawOne(cells1, celln1, 0, 0);   // coordinates are offsets for R
+  // drawOne(cells1, celln1, 0, 0);   // coordinates are offsets for R
 
-  pg_bg.stroke(0);
-  pg_bg.fill(255);
-  strokeWeight(20);
-  pg_text.text('A-G \nV.1', (canvasSize_w/8), 3*(canvasSize_h/8) );
-  //text('AMUSE \nGUEULES \nVOL. 1', canvasSize_w/2, canvasSize_h/2);
-  // text('Amuse \nGueules \nVol One', canvasSize_w/2, canvasSize_h/2);
-
-  // stroke(255);
-  // fill(0);
-  // strokeWeight(5);
-  // text('A-G \nVOL. 1', canvasSize_w/2, canvasSize_h/2);
-
-  pg_text_img.copy(pg_text, 0, 0, pg_text.width, pg_text.height, 0, 0, pg_text.width, pg_text.height);
-  pg_bg_img.copy(pg_bg, 0, 0, pg_bg.width, pg_bg.height, 0, 0, pg_bg.width, pg_bg.height);
-
-
-  pg_bg_img.mask(pg_text_img);
-
-  // image(pg_text_img, 0, 0);
-  image(pg_bg_img, 0, 0);
-
+  drawOne(cells1, celln1, random(0,100), random(0,100));   // coordinates are offsets for R
+  drawOne(cells1, celln1, random(100,200), random(100,200));   // coordinates are offsets for R
 }
 
 function ligneAvec(strokeWeight_thickness, lineStrokeColor_r, lineStrokeColor_g, lineStrokeColor_b ) {
-  pg_bg.strokeWeight(strokeWeight_thickness);
-  pg_bg.stroke(lineStrokeColor_r, lineStrokeColor_g, lineStrokeColor_b);
+  strokeWeight(strokeWeight_thickness);
+  stroke(lineStrokeColor_r, lineStrokeColor_g, lineStrokeColor_b);
 }
 
 function drawOne(cells, celln, translateX, translateY){
 
-  pg_bg.push(); // Start a new drawing state
+  push(); // Start a new drawing state
 
-  pg_bg.translate(translateX, translateY);
-
-  // draw the main shape first so the background for the grid isn't drawing transparent
-  // main shape stroke color & weight
-  ligneAvec(18, 0, 0, 0);
+  translate(translateX, translateY);  
+  ligneAvec(1, 0, 0, 0); // for letter background set stroke style
 
   // Draw circumscribing-square
-  pg_bg.fill(255);                  // white
-  pg_bg.rect(0, 0, sq_dim, sq_dim); // sq_dim x sq_dim
-  pg_bg.noFill();
-
-
-  // for letter background set stroke style
-  // ligneAvec(7, 99, 40, 37);
-  ligneAvec(7, 59, 59, 59);  // grey
-
+  fill(255);
+  rect(0, 0, sq_dim, sq_dim); // sq_dim x sq_dim
+  
   // draw bg cell grid
   for (var i = 0; i < celln; i++)
     for (var j = 0; j < celln; j++){
@@ -140,16 +80,78 @@ function drawOne(cells, celln, translateX, translateY){
       cells[i][j].display();
     }
 
-  // // redraw rect shape, so the background lines don't show. Ugh
-  ligneAvec(18, 0, 0, 0);
+  fill(255); // white
 
-  // Draw circumscribing-square
-  pg_bg.rect(0, 0, sq_dim, sq_dim); // sq_dim x sq_dim
-  pg_bg.fill(255);                  // white
+  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////
+  // rect 3 (bottom rect)
+  rect3_height = random(sq_dim/15, (sq_dim/10)*3);
+  rect3_width = random(sq_dim/10, sq_dim - (2*margin));  // max is sq_dim - some margin
 
-  print("mouseX: " + mouseX, mouseY)
+  ligneAvec(1, 0, 0, 0);
+  rect((sq_dim - rect3_width)/2, sq_dim - rect3_height, rect3_width, rect3_height);
+
+  rect1_low_range = 10;
+  rect1_high_range = sq_dim - rect3_height - margin;
+  // Randomly select a bounding RED line for rect1
+  // Use rect3's top + a margin as the range to select in
+  // rect1_lower_bound_y = random(80, sq_dim - rect3_height - margin);
+  rect1_lower_bound_y = random(rect1_low_range, rect1_high_range);
   
-  pg_bg.pop(); // Restore original state
+  // Vertical line connecting R lines
+  ligneAvec(1, 0, 0, 0);
+  line((sq_dim - rect3_width)/2, sq_dim, (sq_dim - rect3_width)/2, rect1_lower_bound_y);
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////
+  // rect 1 (left top/rect2)
+  rect1_yoffset_from_top = 0;
+  do {
+    rect1_yoffset_from_top = random(margin, rect1_lower_bound_y);
+  } while (rect1_lower_bound_y - rect1_yoffset_from_top < margin);
+
+  rect1_width = random((sq_dim - rect3_width)/2, sq_dim - (2*margin));
+
+  rect1_height = rect1_lower_bound_y - rect1_yoffset_from_top
+  rect1_x_start = 0;
+  do {
+    rect1_x_start = random(margin, (sq_dim - rect3_width)/2);
+    sum = rect1_width + rect1_x_start;
+  } while (sum + margin > sq_dim);
+
+  console.log("rect1_height: " + rect1_height);
+
+  ligneAvec(1, 0, 0, 0);
+  rect(rect1_x_start, rect1_yoffset_from_top, rect1_width, rect1_height);
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////
+  // rect 2 (right rect1/rect3) # 200 needs to be anchored to whatever rect1 is doing
+  rect2_x_start = random((sq_dim - rect3_width)/2 + margin, sq_dim - margin);
+  // rect2_y_start = random(rect1_lower_bound_y, sq_dim - rect3_height);
+  rect2_y_start = rect1_lower_bound_y + margin;
+
+  // fills out the rest of the x dimension (sq_dim), anchoring rect2 to right side
+  rect2_width = sq_dim - rect2_x_start;  
+  // rect2_height = random(sq_dim - rect1_lower_bound_y, sq_dim - rect1_high_range);
+  rect2_height = rect1_high_range - rect1_lower_bound_y - margin;
+
+  if (rect2_height < margin){
+    console.log("[SIZE ERROR] heads up broken");
+    // console.log(rect2_y_start + rect2_height + margin);
+    console.log(rect1_high_range);
+    console.log(rect2_height);
+
+  } else {
+
+  ligneAvec(1, 0, 0, 0);
+  rect(rect2_x_start, rect2_y_start, rect2_width, rect2_height);
+  noFill();
+  }
+
+  pop(); // Restore original state
 }
 
 
@@ -197,20 +199,6 @@ class Cell {
   }
 
   display() {
-
-    let dice = random(0, 30)
-    if (Math.round(dice) == 1){
-      let r = random(150,255)
-      let g = random(0,50)
-      let b = random(0, 50)
-      ligneAvec(7, r, g, b);  // reddish
-    }
-    else {
-      let color = random(0,70)
-      ligneAvec(7, color, color, color);  // grey
-      // ligneAvec(7, random(0,50), random(0,50), random(0,50));  // grey
-    }
-
     if (this.moving) this.pos += this.speed;
 
     if (this.pos > this.s) {
@@ -223,15 +211,15 @@ class Cell {
       break;
 
     case 1: 
-      pg_bg.line(this.x, this.y, this.x+this.s, this.y+this.s);
-      pg_bg.line(this.x+this.s/2, this.y, this.x+this.s, this.y+this.s/2);
-      pg_bg.line(this.x, this.y+this.s/2, this.x+this.s/2, this.y+this.s);
+      line(this.x, this.y, this.x+this.s, this.y+this.s);
+      line(this.x+this.s/2, this.y, this.x+this.s, this.y+this.s/2);
+      line(this.x, this.y+this.s/2, this.x+this.s/2, this.y+this.s);
       //break;
 
     case 2: 
-      pg_bg.line(this.x+this.s, this.y, this.x, this.y+this.s);
-      pg_bg.line(this.x+this.s/2, this.y, this.x, this.y+this.s/2);
-      pg_bg.line(this.x+this.s/2, this.y+this.s, this.x+this.s, this.y+this.s/2);
+      line(this.x+this.s, this.y, this.x, this.y+this.s);
+      line(this.x+this.s/2, this.y, this.x, this.y+this.s/2);
+      line(this.x+this.s/2, this.y+this.s, this.x+this.s, this.y+this.s/2);
       break;
     }
   }
