8fd7ff871dbf3c50651aa172e3986369342640ad
ruohoruotsi <ruoho.ruotsi@gmail.com>
2020-04-11 19:50:35 -0700

    [ADD] checkpoint for balanced, correctly positioned, not over-rotated Rs, with a color palette that still needs work

--- generative_logotype/p5/RR001/sketch.js
+++ generative_logotype/p5/RR001/sketch.js
@@ -6,23 +6,24 @@
  * @author : ruoho ruotsi
  */
 
-
 // -----------------------------------------------------------------
 // setup RR
 // -----------------------------------------------------------------
 
 var multiplier = 1;
-var sq_dim = 30; // 20-60?
+// var sq_dim = 30; // 20-60?
 var canvasSize_w = 800 * multiplier;  // 1080p at retina // 1920×1080 
 var canvasSize_h = 800 * multiplier;  // 1080p at retina // 1920×1080 
 
-var celln1 = 6;
-var cells1 = [];
+
 
 var DEBUG = false;  // control debug logging and diagnostic lines
-var margin = sq_dim/18;
-// -----------------------------------------------------------------
 
+// -----------------------------------------------------------------
+let url = "https://coolors.co/app/9e9e9e-adadad-bebebe-d1d1d1-e8e8e8"
+// let url = "https://coolors.co/app/ffffff-f7f7f7-efefef-e8e8e8-e0e0e0" // lightgreyness
+
+let pallete;
 
 let boardRadius = 10; //radius of hex grid
 let size; //size of hexes
@@ -31,9 +32,9 @@ let hexes = [];
 let mainLayout;
 let graphics;
 
-let pallete = ["#0A0311", "#3E1C4B", "#13065C", "#0B089A", "#A922B0", "#D54705", "#76A6A9", "#2A34AF"];
 
 function setup() {
+
   createCanvas(1500, 1500);
   colorMode(HSB, 360, 100, 100, 100);
   angleMode(DEGREES);
@@ -55,36 +56,26 @@ function setup() {
     graphics.ellipse(x, y, w, h);
   }
 
-// -----------------------------------------------------------------
-
-    // setup cell grid1
-  for (var i = 0; i < celln1; i++) {
-      cells1[i] = [];
-      for (var j = 0; j < celln1; j++) {
-          cells1[i][j] = new Cell(i*(sq_dim/celln1), j*(sq_dim/celln1), sq_dim/celln1, 0);
-          cells1[i][j].rand();
-      }
-  }
+  // IOHAVOC
+  pallete = createPallete(url);
 
 }
+
 
 function draw() {
   background(0, 0, 95);
   image(graphics, 0, 0);
 
-
   push();
   translate(width / 2, height / 2);
-  //hexDrawArray(mainLayout, hexes, color(0, 50, 100));
-  // hexDebugGrid(mainLayout, hexes);
 
   // noStroke();
   let i = 0;
-  drawingContext.shadowColor = color(0, 0, 0, 33);
+  drawingContext.shadowColor = color(0, 0, 0, 35);
   drawingContext.shadowBlur = width / 50;
 
   for (let h of hexes) {
-    // print(h);
+    print(h);
     let points = hexGetCorners(mainLayout, h);
     let center = createVector(0, 0);
     for (let p of points) {
@@ -99,11 +90,13 @@ function draw() {
     let distance = dist(points[0].x, points[0].y, center.x, center.y);
 
     push();
-    translate(center.x, center.y);
-    rotate(random(360));
+    translate(center.x, center.y + 150);  // IOHAVOC shift down from center
+    rotate(random(270, 360));
     let n = int(random(points.length));
-    let nn = int(random(5, 15));
+    let nn = int(random(1, 10));
     let isCircle = random(100) > 50 ? true : false;
+    console.log("nn: " + nn);
+    console.log("n: " + n);
 
     for (let m = nn; m > 0; m--) {
       let r = map(sqrt(sq(h.q) + sq(h.s) + sq(h.r)), 0, 8, 5, 0);
@@ -114,27 +107,20 @@ function draw() {
       let p4 = createVector(points[(n + 4) % points.length].x, points[(n + 4) % points.length].y).mult(q);
       
       if (isCircle) {
-        // arc(0, 0, distance / 3 * m / nn, distance / 3 * m / nn, 0, 90, PIE);
-      drawOne(cells1, celln1, random(0,100), random(0,100));   // coordinates are offsets for R
+      // arc(0, 0, distance / 3 * m / nn, distance / 3 * m / nn, 0, 90, PIE);
+
+      drawOne(random(20,30), random(0,100), random(0,100));   // coordinates are offsets for R
 
       } 
-      // else {
-      //   if (n % 2 == 0) {
-      //     beginShape();
-      //     vertex(p1.x * m / nn, p1.y * m / nn);
-      //     vertex(p2.x * m / nn, p2.y * m / nn);
-      //     vertex(p3.x* m/nn,p3.y* m/nn);      
-      //     vertex(p4.x * m / nn, p4.y * m / nn);
-      //     endShape(CLOSE);
-      //   } else {
-      //     beginShape();
-      //     vertex(p1.x * m/nn,p1.y* m/nn);      
-      //     vertex(p2.x * m / nn, p2.y * m / nn);
-      //     vertex(p3.x * m / nn, p3.y * m / nn);
-      //     vertex(p4.x * m / nn, p4.y * m / nn);
-      //     endShape(CLOSE);
-      //   }
-      // }
+      else {
+        if (n % 2 == 0) {
+          drawOne(random(20, 40), random(0, 100), random(0, 100));   // coordinates are offsets for R
+
+        } else {
+          
+          drawOne(random(30, 60), random(0, 100), random(0, 100));   // coordinates are offsets for R
+        }
+      }
     }
 
     beginShape();
@@ -151,52 +137,46 @@ function draw() {
 }
 
 
-
-
-
-
-// function setup() {
-//   createCanvas(canvasSize_w, canvasSize_h);
-//   frameRate(1); // Attempt to refresh at starting FPS // 0.5
-
-
-//   noFill();
-//   if (DEBUG) {
-//     noLoop();
-//   }
-// }
-
-
-function draw_r() {
-
-  // clear background on each frame
-  background(255, 255, 255);      // <=== white background
-  clear();                        // <=== transparent background
-
-  // draw one here
-  pos1_w = (canvasSize_w - sq_dim) / 2 ;
-  pos_h = (canvasSize_h - sq_dim) / 2;
-  // drawOne(cells1, celln1, 0, 0);   // coordinates are offsets for R
-
-  drawOne(cells1, celln1, random(0,100), random(0,100));   // coordinates are offsets for R
-  drawOne(cells1, celln1, random(100,200), random(100,200));   // coordinates are offsets for R
-}
-
 function ligneAvec(strokeWeight_thickness, lineStrokeColor_r, lineStrokeColor_g, lineStrokeColor_b ) {
   strokeWeight(strokeWeight_thickness);
   stroke(lineStrokeColor_r, lineStrokeColor_g, lineStrokeColor_b);
 }
 
-function drawOne(cells, celln, translateX, translateY){
+function drawOne(square_dim, translateX, translateY){
+
+  var celln = 6;
+  if (square_dim < 20) {
+    celln = 4;
+  }
+  else if (square_dim < 40 ) {
+    celln = 6;
+  }
+  else {
+    celln = 10;
+  }
+
+  // var celln = 10;
+  var cells = [];
+  var margin = square_dim/18;
+
+  // setup cell grid1
+  for (var i = 0; i < celln; i++) {
+      cells[i] = [];
+      for (var j = 0; j < celln; j++) {
+          cells[i][j] = new Cell(i*(square_dim/celln), j*(square_dim/celln), square_dim/celln, 0);
+          cells[i][j].rand();
+      }
+  }
 
   push(); // Start a new drawing state
 
   translate(translateX, translateY);  
   ligneAvec(1, 0, 0, 0); // for letter background set stroke style
 
+  let c1 = random(pallete)
   // Draw circumscribing-square
   fill(255);
-  rect(0, 0, sq_dim, sq_dim); // sq_dim x sq_dim
+  rect(0, 0, square_dim, square_dim); // sq_dim x sq_dim
   
   // draw bg cell grid
   for (var i = 0; i < celln; i++)
@@ -205,19 +185,19 @@ function drawOne(cells, celln, translateX, translateY){
       cells[i][j].display();
     }
 
-  fill(255); // white
+  fill(c1); // white
 
   //////////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////
   // rect 3 (bottom rect)
-  rect3_height = random(sq_dim/15, (sq_dim/10)*3);
-  rect3_width = random(sq_dim/10, sq_dim - (2*margin));  // max is sq_dim - some margin
+  rect3_height = random(square_dim/15, (square_dim/10)*3);
+  rect3_width = random(square_dim/10, square_dim - (2*margin));  // max is sq_dim - some margin
 
   ligneAvec(1, 0, 0, 0);
-  rect((sq_dim - rect3_width)/2, sq_dim - rect3_height, rect3_width, rect3_height);
+  rect((square_dim - rect3_width)/2, square_dim - rect3_height, rect3_width, rect3_height);
 
   rect1_low_range = 10;
-  rect1_high_range = sq_dim - rect3_height - margin;
+  rect1_high_range = square_dim - rect3_height - margin;
   // Randomly select a bounding RED line for rect1
   // Use rect3's top + a margin as the range to select in
   // rect1_lower_bound_y = random(80, sq_dim - rect3_height - margin);
@@ -225,7 +205,7 @@ function drawOne(cells, celln, translateX, translateY){
   
   // Vertical line connecting R lines
   ligneAvec(1, 0, 0, 0);
-  line((sq_dim - rect3_width)/2, sq_dim, (sq_dim - rect3_width)/2, rect1_lower_bound_y);
+  line((square_dim - rect3_width)/2, square_dim, (square_dim - rect3_width)/2, rect1_lower_bound_y);
 
 
   //////////////////////////////////////////////////////////////////////////////////
@@ -236,14 +216,14 @@ function drawOne(cells, celln, translateX, translateY){
     rect1_yoffset_from_top = random(margin, rect1_lower_bound_y);
   } while (rect1_lower_bound_y - rect1_yoffset_from_top < margin);
 
-  rect1_width = random((sq_dim - rect3_width)/2, sq_dim - (2*margin));
+  rect1_width = random((square_dim - rect3_width)/2, square_dim - (2*margin));
 
   rect1_height = rect1_lower_bound_y - rect1_yoffset_from_top
   rect1_x_start = 0;
   do {
-    rect1_x_start = random(margin, (sq_dim - rect3_width)/2);
+    rect1_x_start = random(margin, (square_dim - rect3_width)/2);
     sum = rect1_width + rect1_x_start;
-  } while (sum + margin > sq_dim);
+  } while (sum + margin > square_dim);
 
   console.log("rect1_height: " + rect1_height);
 
@@ -254,12 +234,12 @@ function drawOne(cells, celln, translateX, translateY){
   //////////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////
   // rect 2 (right rect1/rect3) # 200 needs to be anchored to whatever rect1 is doing
-  rect2_x_start = random((sq_dim - rect3_width)/2 + margin, sq_dim - margin);
+  rect2_x_start = random((square_dim - rect3_width)/2 + margin, square_dim - margin);
   // rect2_y_start = random(rect1_lower_bound_y, sq_dim - rect3_height);
   rect2_y_start = rect1_lower_bound_y + margin;
 
   // fills out the rest of the x dimension (sq_dim), anchoring rect2 to right side
-  rect2_width = sq_dim - rect2_x_start;  
+  rect2_width = square_dim - rect2_x_start;  
   // rect2_height = random(sq_dim - rect1_lower_bound_y, sq_dim - rect1_high_range);
   rect2_height = rect1_high_range - rect1_lower_bound_y - margin;
 
@@ -295,6 +275,16 @@ function mouseReleased() {
 function keyPressed() {
   if (key == 's' || key == 'S') 
     saveCanvas(Math.round(new Date().getTime() / 1000).toString(), 'png');
+}
+
+function createPallete(_url) {
+  let slash_index = _url.lastIndexOf('/');
+  let pallate_str = _url.slice(slash_index + 1);
+  let arr = pallate_str.split('-');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = '#' + arr[i];
+  }
+  return arr;
 }
 
 
