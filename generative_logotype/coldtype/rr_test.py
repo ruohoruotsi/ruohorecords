39884563223ca73f6ba1ec5cb75b4f7996bafdc8
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-10-03 23:38:04 -0700

    [ADD] coldtype exercises, Oct 3

--- generative_logotype/coldtype/rr_test.py
+++ generative_logotype/coldtype/rr_test.py
@@ -1,5 +1,8 @@
 from coldtype import *
 
+
+# pass a rect to an oval, with frame? taking a color tuple
+# what is a DATPen
 @renderable()
 def test(r):
-    return DATPen().oval(r.inset(110)).f(hsl(0.1))
+    return DATPen().oval(r).f((0.1,0.2,0.6))
