7fae6d4f642e371157610ce040d8589f5af164dd
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-10-05 14:38:13 -0700

    [ADD] multiple renderables exercise

--- generative_logotype/coldtype/rr_test.py
+++ generative_logotype/coldtype/rr_test.py
@@ -12,9 +12,21 @@ from coldtype import *
 
 
 #######################################################################
+# Multiple Renderables
+# Built in fonts are ColdtypeObviously, RecursiveMono, MutatorSans
+@renderable((1440, 1080))
+def sample_text(r):
+    return PS([
+        P().oval(r.inset(20)).f(hsl(random())),
+        (StSt("RUOHO",
+        Font.))
+        ])
+
+
+#######################################################################
 # What is P()? f.a.r = frame.animation.rect, black, inset 300pixels
 # with sinusoidal easing in/out
-@animation(timeline=10) # duration of 10 frames
-def easing_example(f):
-    square = P(f.a.r.inset(300)).f(0)
-    return square.rotate(f.e("seio", 1, rng=(-3, 2)))
+# @animation(timeline=10) # duration of 10 frames
+# def easing_example(f):
+#     square = P(f.a.r.inset(300)).f(0)
+#     return square.rotate(f.e("seio", 1, rng=(-3, 2)))
