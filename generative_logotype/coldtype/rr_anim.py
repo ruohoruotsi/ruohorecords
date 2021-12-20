868accba72780f198fbc325f0418ff2bf7d80af0
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-12-20 12:23:50 -0800

    [ADD] basic logo animation

--- generative_logotype/coldtype/rr_anim.py
+++ generative_logotype/coldtype/rr_anim.py
@@ -1,13 +1,17 @@
 from coldtype import *
 
-@animation((250, 250), bg=0, timeline=24)
-def flyinga(f):
-    return PS([
-        (P().rect(f.a.r)
-            .f(hsl(f.e("qeio", 0)))),
-        (StSt("A", Font.MutatorSans(),
-            50, wght=0.2)
-            .align(f.a.r)
-            .scale(f.e("eei", 0, rng=(1, 51)))
-            .rotate(f.e("qeio", 0, rng=(0, 360)))
-            .f(1))])
+rr_ttf = Font("rr_fonts/lavirint/lavirint.ttf")
+
+@animation((1080, 540), timeline=60, storyboard=[30], render_bg=1)
+def vari(f):
+    return (StSt("R", rr_ttf,
+        font_size=f.e("seio", 1, rng=(50, 150)),
+        # wdth=f.e("eeio", 1, rng=[0, 10]),
+        # wght=f.e("seio", 1, rng=[0,18]),
+        leading=5)
+        .f(0)
+        .understroke(s=0, sw=1)  # OMG what is this??
+        .align(f.a.r))
+
+# Shift space to render
+release = vari.export("h264")
