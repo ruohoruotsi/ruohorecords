4bb70f53a410b0f3f3c15f8fadec9738c531fb42
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-12-21 21:15:27 -0800

    [ADD] understroked animation with motion along <<tu>> axis ...starting to look good

--- generative_logotype/coldtype/rr_anim.py
+++ generative_logotype/coldtype/rr_anim.py
@@ -1,17 +1,60 @@
 from coldtype import *
+from coldtype.fx.skia import phototype
 
 rr_ttf = Font("rr_fonts/lavirint/lavirint.ttf")
 
-@animation((1080, 540), timeline=60, storyboard=[30], render_bg=1)
-def vari(f):
-    return (StSt("RUOHO RECORDS", rr_ttf,
-        font_size=f.e("seio", 1, rng=(20, 80)),
-        # wdth=f.e("eeio", 1, rng=[0, 10]),
-        # wght=f.e("seio", 1, rng=[0,18]),
-        leading=5)
-        .f(0)
-        .understroke(s=0, sw=1)  # OMG what is this??
-        .align(f.a.r))
+# @animation((1080, 250), timeline=60, storyboard=[30], render_bg=1)
+# def vari(f):
 
-# Shift space to render
-release = vari.export("h264")
+#     return PS([
+#             (P(f.a.r)
+#                 .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
+
+#     (StSt("RUOHO RECORDS", rr_ttf,
+#         font_size=f.e("seio", 1, rng=(20, 80)),
+#         # wdth=f.e("eeio", 1, rng=[0, 10]),
+#         # wght=f.e("seio", 1, rng=[0,18]),
+#         leading=5)
+#         .f(0)
+#         .understroke(s=0, sw=1)  # OMG what is this??
+#         .align(f.a.r))
+#     ])
+
+# # Shift space to render
+# release = vari.export("h264")
+
+# Note, if you don't return a color canvas, then the type will be 
+# rendered on transparent background
+# @animation((1000, 250), timeline=60, bg=hsl(0.42, 0.37, 0.16))
+# def gw2(f):
+#     def styler(g):
+#         fa = f.adj(-g.i*3)
+#         return [
+#             Style(rr_ttf, 80),
+#             dict(font_size=f.e("seio", 1, rng=(40, 70)))
+#         ]
+#     return (Glyphwise("RUOHO RECORDS", styler)
+#         .align(f.a.r, th=0)
+#         .f(1))
+
+# release = gw2.export("h264")
+
+
+@animation((1000, 250),
+    bg=hsl(0.45, 0.65, 0.4),
+    timeline=Timeline(30, 18),
+    storyboard=[0, 15])
+def understroke(f):
+    return (StSt("RUOHO RECORDS", rr_ttf,
+        ro=1,
+        font_size=70,
+        rotate=f.e("ceio", 1, rng=(-10, 0)),
+        tu=f.e("eeio", 1, rng=(100, -255)))
+        .align(f.a.r)
+        .reverse_pens() # overlaps pens L->R
+        .f(1)
+        .understroke(sw=15)
+        .ch(phototype(f.a.r, blur=2, 
+                            cut=160, 
+                            cutw=30, 
+                            fill=hsl(0.17, 1))))
