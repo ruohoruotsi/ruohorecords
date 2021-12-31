8ea95ca15120f6b4f54506bc3914b73637ef9cc8
ruohoruotsi <ruoho.ruotsi@gmail.com>
2021-12-31 13:56:01 -0800

    [CLEANUP] anim a bit, now that splash has been forked

--- generative_logotype/coldtype/rr_anim.py
+++ generative_logotype/coldtype/rr_anim.py
@@ -3,25 +3,25 @@ from coldtype.fx.skia import phototype
 
 rr_ttf = Font("rr_fonts/lavirint/lavirint.ttf")
 
-# @animation((1080, 250), timeline=60, storyboard=[30], render_bg=1)
-# def vari(f):
+@animation((1080, 250), timeline=60, storyboard=[30], render_bg=1)
+def vari(f):
 
-#     return PS([
-#             (P(f.a.r)
-#                 .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
+    return PS([
+            (P(f.a.r)
+                .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
 
-#     (StSt("RUOHO RECORDS", rr_ttf,
-#         font_size=f.e("seio", 1, rng=(20, 80)),
-#         # wdth=f.e("eeio", 1, rng=[0, 10]),
-#         # wght=f.e("seio", 1, rng=[0,18]),
-#         leading=5)
-#         .f(0)
-#         .understroke(s=0, sw=1)  # OMG what is this??
-#         .align(f.a.r))
-#     ])
+    (StSt("RUOHO RECORDS", rr_ttf,
+        font_size=f.e("seio", 1, rng=(20, 80)),
+        # wdth=f.e("eeio", 1, rng=[0, 10]),
+        # wght=f.e("seio", 1, rng=[0,18]),
+        leading=5)
+        .f(0)
+        .understroke(s=0, sw=1)  # OMG what is this??
+        .align(f.a.r))
+    ])
 
-# # Shift space to render
-# release = vari.export("h264")
+# Shift space to render
+release = vari.export("h264")
 
 # Note, if you don't return a color canvas, then the type will be 
 # rendered on transparent background
@@ -77,20 +77,20 @@ rr_ttf = Font("rr_fonts/lavirint/lavirint.ttf")
 #                             fill=hsl(h=0.5, s=0.5, l=0.0))))
 
 
-@animation((2048, 1152),
-    bg=hsl(0.45, 0.65, 1.0),
-    timeline=Timeline(30, 18))
-def youtube_header_banner(f):
-    return (StSt("RUOHO RECORDS", rr_ttf,
-        ro=1,
-        font_size=116,
-        rotate=f.e("ceio", 1, rng=(-10, 0)),
-        tu=f.e("eeio", 1, rng=(100, -100)))
-        .align(f.a.r)
-        .reverse_pens() # overlaps pens L->R
-        .f(1)
-        .understroke(sw=10)
-        .ch(phototype(f.a.r, blur=2, 
-                            cut=110, 
-                            cutw=30, 
-                            fill=hsl(h=0.5, s=0.5, l=0.0))))
+# @animation((2048, 1152),
+#     bg=hsl(0.45, 0.65, 1.0),
+#     timeline=Timeline(30, 18))
+# def youtube_header_banner(f):
+#     return (StSt("RUOHO RECORDS", rr_ttf,
+#         ro=1,
+#         font_size=116,
+#         rotate=f.e("ceio", 1, rng=(-10, 0)),
+#         tu=f.e("eeio", 1, rng=(100, -100)))
+#         .align(f.a.r)
+#         .reverse_pens() # overlaps pens L->R
+#         .f(1)
+#         .understroke(sw=10)
+#         .ch(phototype(f.a.r, blur=2, 
+#                             cut=110, 
+#                             cutw=30, 
+#                             fill=hsl(h=0.5, s=0.5, l=0.0))))
