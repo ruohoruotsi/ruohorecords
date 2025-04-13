e9b5558b8dc9435216dc5d5eda244bba48b8a9bf
ruohoruotsi <ruoho.ruotsi@gmail.com>
2025-04-13 10:43:10 -0700

    [FIX] up various logotype animation - checkpoint

--- generative_logotype/coldtype/rr_anim.py
+++ generative_logotype/coldtype/rr_anim.py
@@ -4,21 +4,24 @@ from coldtype.fx.skia import phototype
 rr_ttf = Font("rr_fonts/lavirint/lavirint.ttf")
 
 #----------------------------------------------------------------------
-@animation((1080, 250), timeline=60, render_bg=1)
-def vari(f):
-    return P([
-            (P(f.a.r)
-                .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
-
-    (StSt("RUOHO RECORDS", rr_ttf,
-        font_size=f.e("seio", 1, rng=(20, 80)),
-        # wdth=f.e("eeio", 1, rng=[0, 10]),
-        # wght=f.e("seio", 1, rng=[0,18]),
-        leading=5)
-        .f(0)
-        .understroke(s=0, sw=1)  # OMG what is this??
-        .align(f.a.r))
-    ])
+@animation((1080, 180), timeline=60, render_bg=1)
+def rr_colour_pulse_2secs(f):
+    return P(
+            [
+                (P(f.a.r)
+                    .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))
+                    ),
+                (StSt("RUOHO RECORDS", rr_ttf,
+                    font_size=f.e("eeio", 1, rng=(85, 92)),
+                    #wdth=f.e("eeio", 1, rng=[0, 90]),
+                    #wght=f.e("seio", 1, rng=[0, 18]), # Lavrint doesn't have variable weight
+                    leading=5)
+                    .f(0)
+                    .understroke(s=0, sw=1)  # OMG what is this??
+                    .align(f.a.r)
+                )
+            ]
+        )
 # # Shift space to render
 # release = vari.export("h264")
 
