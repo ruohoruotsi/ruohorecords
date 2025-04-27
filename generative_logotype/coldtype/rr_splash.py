18baa95d38d1350d8a0d10470e57a16247b047e5
ruohoruotsi <ruoho.ruotsi@gmail.com>
2025-04-27 00:41:45 -0700

    [ADD] splash of colour, and more phototype

--- generative_logotype/coldtype/rr_splash.py
+++ generative_logotype/coldtype/rr_splash.py
@@ -8,22 +8,22 @@ rr_ttf = Font.Cacheable(rr_fonts_path + "lavirint/lavirint.ttf")
 def rr_splash(f):
 
     return P([
-            # splash of colour
-            # (P(f.a.r)
-            #     .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
+            #splash of colour
+            (P(f.a.r)
+                .f(hsl(f.e("eeio", 1, rng=(0.1, 0.2)), 0.8, 0.6))),
 
             StSt("RUOHO RECORDS", rr_ttf,
                 ro=1,
                 font_size=f.e("seio", 1, rng=(1, 2920)),
-                tu=f.e("eeio", 1, rng=(100, -100)))
+                tu=f.e("seio", 1, rng=(100, -100)))
             .align(f.a.r)
             .reversePens() # overlaps pens L->R
             .f(1)
-            .understroke(sw=10)
+            .understroke(sw=40 )
             .ch(phototype(f.a.r, 
                         blur=2, 
-                        cut=100, 
-                        cutw=30, 
+                        cut=110, 
+                        cutw=90, 
                         fill=hsl(h=0.5, s=0.5, l=0.0)
                         )
                 )
